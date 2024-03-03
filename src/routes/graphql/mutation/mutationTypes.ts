a5470b85b4b6e26f84519d0b1ad9dad8919c4672
Mikhail <nodeprog@gmail.com>
2024-03-03 21:20:01 +0300

    feat: add mutation deletePost

--- src/routes/graphql/mutation/mutationTypes.ts
+++ src/routes/graphql/mutation/mutationTypes.ts
@@ -5,6 +5,7 @@ import { deleteUser } from './deleteUser.js';
 import { createPost } from './createPost.js';
 import { changePost } from './changePost.js';
 import { subscribeTo } from './subscribeTo.js';
+import { deletePost } from './deletePost.js';
 import { unsubscribeFrom } from './unsubscribeFrom.js';
 
 export const mutationTypes = new GraphQLObjectType({
@@ -15,6 +16,7 @@ export const mutationTypes = new GraphQLObjectType({
     ...deleteUser,
     ...createPost,
     ...changePost,
+    ...deletePost,
     ...subscribeTo,
     ...unsubscribeFrom,
   }),
